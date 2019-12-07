63f7585d387d7ceee29ca010cbb5f95034df98e1
singcl <24661881@qq.com>
2019-12-08 02:24:13 +0800

    :art: 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 echo '部署前端'
 cd ./example/front
-cnpm i
+
+echo '安装前端依赖...'
+npm i
+
+echo 'build前端...'
 npm run build
 echo '前端部署完成'
 
-echo '启动node服务器'
 cd ./../../
+echo '安装服务端依赖...'
+npm i
+
+echo '启动node服务器'
 pm2 restart pm2.config.js
